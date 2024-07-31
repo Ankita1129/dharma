047d0b06576d34dc08afe2ddacae524d72b8d2f8
Ankita Singh <88998746+Ankita1129@users.noreply.github.com>
2024-08-01 01:07:33 +0530

    Update Footer.js

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <h5 className="text-center">Made with &#10084; by @vidhisorte</h5>
+      <h5 className="text-center">Made with &#10084; by @ankitasingh</h5>
       <p className="text-center">
         <Link to="/about">About us</Link>|<Link to="/contact">Contact</Link>|
         <Link to="/">Home</Link>
